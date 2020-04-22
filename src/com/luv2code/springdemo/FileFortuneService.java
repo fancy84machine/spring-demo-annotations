3de0ae457c3551e1e1f11f332869f253b2bbeeb7
Ling Sang <lingsang.tse@mail.utoronto.ca>
2020-04-21 21:12:55 -0400

    chapter 8 practice

---
+++ src/com/luv2code/springdemo/FileFortuneService.java
@@ -0,0 +1,55 @@
+package com.luv2code.springdemo;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FileFortuneService implements FortuneService {
+
+	private String fileName = "C:/Users/Ling Sang 2/eclipse-workspace/foobar/fortune-data";
+	private List<String> theFortunes;
+
+	private Random myRandom = new Random();
+
+	public FileFortuneService() {
+
+		File theFile = new File(fileName);
+
+		System.out.println("Reading fortunes from file: " + theFile);
+		System.out.println("File exists: " + theFile.exists());
+
+		// initialize array list
+		theFortunes = new ArrayList<String>();
+
+		// read fortunes from file
+		try (BufferedReader br = new BufferedReader(new FileReader(theFile))) {
+
+			String tempLine;
+
+			while ((tempLine = br.readLine()) != null) {
+				theFortunes.add(tempLine);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public String getFortune() {
+		// pick a random string from the array
+		int index = myRandom.nextInt(theFortunes.size());
+
+		String tempFortune = theFortunes.get(index);
+
+		return tempFortune;
+	}
+
+}
